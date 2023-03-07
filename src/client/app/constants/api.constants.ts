fac0e0084e4a8dc36f50d73f8391171700a03cbf
nk11dev <nk11dev@gmail.com>
2023-03-08 00:49:11 +0300

    (env) Added "API_PATH" environment variable support.

--- src/client/app/constants/api.constants.ts
+++ src/client/app/constants/api.constants.ts
@@ -1,6 +1,3 @@
-// const API_PATH = '/api';
-const API_PATH = '/api-example';
-
 export const API_URL = (process.env.API_HOST)
-  ? `${process.env.API_HOST}${API_PATH}`
-  : API_PATH;
+  ? `${process.env.API_HOST}${process.env.API_PATH}`
+  : process.env.API_PATH;
