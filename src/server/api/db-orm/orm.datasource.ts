4fb2aa5f51d6713fe9a8be4e09515947ddd5bf91
nk11dev <nk11dev@gmail.com>
2023-05-05 07:19:12 +0300

    (dependencies, typeorm, migrations) Added npm scripts for typeorm migrations.

--- src/server/api/db-orm/orm.datasource.ts
+++ src/server/api/db-orm/orm.datasource.ts
@@ -1,9 +1,16 @@
-import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 
 import { Section } from '@/server/api/db-orm/entities/section.entity';
 
 const { env } = process;
+
+const migrationsExt = (process.env.NODE_ENV === 'migration')
+
+  // NODE_ENV 'migration' is settled in npm scripts, that runs typeorm CLI by 'ts-node', so it consumes '.ts' modules
+  ? 'ts'
+
+  // TypeOrm is working in a JavaScript environment, so it’s going to work on our '.ts' files once they are compiled in '.js'
+  : 'js';
 
 const dataSource = new DataSource({
   type: 'postgres',
@@ -14,16 +21,19 @@ const dataSource = new DataSource({
   database: env.PG_DATABASE,
   entities: [Section],
   synchronize: false,
-  logging: false,
+  logging: true,
+  migrations: [
+    `./src/server/api/db-orm/migrations/**/*.${migrationsExt}`,
+  ],
 });
 
 dataSource
   .initialize()
   .then(() => {
-    console.log('Data Source has been initialized!');
+    console.log('--- Data Source has been initialized!');
   })
   .catch((err) => {
-    console.error('Error during Data Source initialization:', err);
+    console.error('--- Error during Data Source initialization:', err);
   });
 
 export default dataSource;
