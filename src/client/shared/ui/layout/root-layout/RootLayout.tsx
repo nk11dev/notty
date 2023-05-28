41afc2a493c8128c0ff1f34598a2a936c2761d2d
nk11dev <nk11dev@gmail.com>
2023-05-28 19:56:48 +0300

    (refactoring - layout) Removed "Header".

--- src/client/shared/ui/layout/root-layout/RootLayout.tsx
+++ src/client/shared/ui/layout/root-layout/RootLayout.tsx
@@ -2,16 +2,12 @@ import styles from './RootLayout.module.scss';
 
 import React from 'react';
 
-import Header from '@/shared/ui/layout/header';
-
 type Props = {
   children: React.ReactNode
 };
 
 const RootLayout = (props: Props) => (
   <div className={styles.wrapper}>
-    <Header />
-
     <section className={styles.content}>
       {props.children}
     </section>
