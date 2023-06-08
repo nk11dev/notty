5b3155b28b4744cae5a857985da1f5be868c803b
nk11dev <nk11dev@gmail.com>
2023-06-08 03:51:50 +0300

    (page - note header) Added "HeaderButton" and stub header buttons for "Add note to favorites / Remove note from favorites" and "Note actions".

--- src/client/features/note-header/NoteHeader.tsx
+++ src/client/features/note-header/NoteHeader.tsx
@@ -1,9 +1,12 @@
 import styles from './NoteHeader.module.scss';
 
 import React from 'react';
+import { HiDotsHorizontal } from 'react-icons/hi';
+import { AiOutlineStar } from 'react-icons/ai';
 
 import { useSectionState } from '@/entities/section/hooks';
 import { useNoteState } from '@/entities/note/hooks';
+import HeaderButton from '@/shared/ui/controls/header-button';
 
 const NoteHeader = () => {
   const sectionState = useSectionState();
@@ -13,6 +16,16 @@ const NoteHeader = () => {
     <div className={styles.element}>
       <div className={styles.breadcrumbs}>
         {sectionState?.title} / {noteState?.title}
+      </div>
+
+      <div className={styles.headerControls}>
+        <HeaderButton isDisabled>
+          <AiOutlineStar />
+        </HeaderButton>
+        
+        <HeaderButton isDisabled>
+          <HiDotsHorizontal />
+        </HeaderButton>
       </div>
     </div>
   );
--- src/client/shared/ui/controls/header-button/HeaderButton.tsx
+++ src/client/shared/ui/controls/header-button/HeaderButton.tsx
@@ -1,4 +1,4 @@
-import styles from './ControlsButton.module.scss';
+import styles from './HeaderButton.module.scss';
 
 import React, { cloneElement } from 'react';
 import type { ReactElement } from 'react';
@@ -9,26 +9,24 @@ import ButtonIcon from '@/shared/ui/controls/button-icon';
 type Props = {
   children?: React.ReactNode,
   clickHandler?: () => void;
-  clsIsActive?: boolean,
   tooltip?: string,
   isDisabled?: boolean,
 };
 
-const ControlsButton = (props: Props) => (
+const HeaderButton = (props: Props) => (
   <ButtonIcon
     clickHandler={props.clickHandler}
-    cls={cn(styles.controlsBtn, {
-      [styles.isActive]: props.clsIsActive,
+    cls={cn(styles.headerBtn, {
       [styles.isDisabled]: (props.isDisabled === true)
     })}
     tooltip={props.tooltip}
   >
     {
       cloneElement(props.children as ReactElement, {
-        className: styles.controlsIcon,
+        className: styles.headerIcon,
       })
     }
   </ButtonIcon>
 );
 
-export default ControlsButton;
+export default HeaderButton;
