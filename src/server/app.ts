e28f2aa0c46a258f30fd3424cc85b71beb9b2db7
nk11dev <nk11dev@gmail.com>
2023-07-01 16:30:16 +0300

    [refactoring]: (server) Moved app-related code to "/server/app" module.

---
+++ src/server/app.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+
+import { headersMiddleware } from '@/server/middlewares';
+import collectNestedRoutes from '@/server/helpers/routing.helpers';
+import foldersRoutes from '@/server/routes/folders.routes';
+import notesRoutes from '@/server/routes/notes.routes';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(headersMiddleware);
+app.use(bodyParser.json());
+app.use(express.static('dist/client'));
+
+// API routes
+app.use('/api/folders', foldersRoutes);
+app.use('/api/notes', notesRoutes);
+
+// App routes (used only for "production" mode for correct HMR working with client "development" mode).
+if (process.env.NODE_ENV == 'production') {
+
+  // Domain routes
+  const appRoutes = require('@/app/routing/Router').appRoutes;
+  const flatRoutes = collectNestedRoutes(appRoutes);
+
+  for (const route of flatRoutes) {
+    app.get(route, (_req, res) => {
+      res.sendFile('dist/client/index.html', { root: '.' });
+    });
+  }
+
+  // 404 route
+  app.use((_req, res) => {
+    res.status(404);
+    res.sendFile('dist/client/index.html', { root: '.' });
+  });
+}
+
+export default app;
--- src/server/index.server.ts
+++ src/server/index.server.ts
@@ -1,44 +1,6 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import logger from 'morgan';
+import colors from 'ansi-colors';
 
-import { headersMiddleware } from '@/server/middlewares';
-import collectNestedRoutes from '@/server/helpers/routing.helpers';
-import foldersRoutes from '@/server/routes/folders.routes';
-import notesRoutes from '@/server/routes/notes.routes';
-
-const colors = require('ansi-colors');
-
-const app = express();
-
-app.use(logger('dev'));
-app.use(headersMiddleware);
-app.use(bodyParser.json());
-app.use(express.static('dist/client'));
-
-// API routes
-app.use('/api/folders', foldersRoutes);
-app.use('/api/notes', notesRoutes);
-
-// App routes (used only for "production" mode for correct HMR working with client "development" mode).
-if (process.env.NODE_ENV == 'production') {
-
-  // Domain routes
-  const appRoutes = require('@/app/routing/Router').appRoutes;
-  const flatRoutes = collectNestedRoutes(appRoutes);
-
-  for (const route of flatRoutes) {
-    app.get(route, (_req, res) => {
-      res.sendFile('dist/client/index.html', { root: '.' });
-    });
-  }
-
-  // 404 route
-  app.use((_req, res) => {
-    res.status(404);
-    res.sendFile('dist/client/index.html', { root: '.' });
-  });
-}
+import app from '@/server/app';
 
 const PORT = process.env.PORT || process.env.PORT_SERVER;
 
