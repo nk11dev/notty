b61bd4791d053fd3f8eec1aae3d08303fd372cec
nk11dev <nk11dev@gmail.com>
2023-07-25 20:17:00 +0300

    [feature - auth]: (rtk - listeners) Added resetting of "authApi", "foldersApi" and "notesApi" to listener with "logoutUser.matchFulfilled" matcher.

--- src/client/entities/user/listeners/user.listeners.ts
+++ src/client/entities/user/listeners/user.listeners.ts
@@ -3,6 +3,8 @@ import { isAnyOf } from '@reduxjs/toolkit';
 import type { AppStartListening } from '@/app/redux/middlewares/listener.middleware';
 import userSlice from '@/entities/user/slices/user.slice';
 import { authApi } from '@/entities/user/slices';
+import { foldersApi } from '@/entities/folder/api-slices';
+import { notesApi } from '@/entities/note/api-slices';
 
 const {
   setAuthUpdating,
@@ -52,5 +54,8 @@ export const listenAuthReset = (startListening: AppStartListening) => startListe
   matcher: logoutUser.matchFulfilled,
   effect: async (_action, listenerApi) => {
     listenerApi.dispatch(resetAuth());
+    listenerApi.dispatch(authApi.util.resetApiState());
+    listenerApi.dispatch(foldersApi.util.resetApiState());
+    listenerApi.dispatch(notesApi.util.resetApiState());
   },
 })
