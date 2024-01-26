c0bb7ac22c585d52d95f548528e9e923e3c15889
nk11dev <nk11dev@gmail.com>
2024-01-27 02:56:23 +0300

    [feature - scrollbar visibility] (dependencies, /shared/ui/controls) Details:
    1) Dependencies:
    - Added "@types/overlayscrollbars" dependency.
    - Bumped "overlayscrollbars" dependencies.
    
    2) Scrollbar:
    - Added "visibility" option.

--- src/client/shared/ui/controls/scrollbar/Scrollbar.tsx
+++ src/client/shared/ui/controls/scrollbar/Scrollbar.tsx
@@ -3,15 +3,26 @@ import styles from './Scrollbar.module.scss';
 import React from 'react';
 import type { ReactNode } from 'react';
 import cn from 'classnames';
+import type { ScrollbarsVisibilityBehavior } from 'overlayscrollbars';
 import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 
 type Props = {
   children: ReactNode,
   cls?: string,
+  options?: {
+    visibility?: ScrollbarsVisibilityBehavior
+  }
 };
 
-const Scroll = ({ children, cls }: Props) => (
-  <OverlayScrollbarsComponent className={cn(styles.element, cls)}>
+const Scroll = ({ children, cls, options }: Props) => (
+  <OverlayScrollbarsComponent
+    className={cn(styles.element, cls)}
+    options={{
+      scrollbars: {
+        visibility: options?.visibility || 'auto'
+      }
+    }}
+  >
     {children}
   </OverlayScrollbarsComponent>
 );
