a9f001198313a61617ebc535629f33add23062ac
nk11dev <nk11dev@gmail.com>
2024-02-01 05:50:36 +0300

    [dependencies - bump] (typeorm) Bumped "typeorm" dependency to "0.3.20".

--- src/server/index.server.ts
+++ src/server/index.server.ts
@@ -5,5 +5,5 @@ import app from '@/server/app';
 const PORT = process.env.PORT || process.env.PORT_SERVER;
 
 const listener = app.listen(PORT, function () {
-  console.log(colors.magenta('\n--- Express app started111'), listener.address(), '\n');
+  console.log(colors.magenta('\n--- Express app started'), listener.address(), '\n');
 });
