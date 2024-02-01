72107bf9578d9836b231d32a7f713f96d3762f26
nk11dev <nk11dev@gmail.com>
2024-02-01 11:49:52 +0300

    [styling] (sidebar) Added styles for correct height of sidebar body if "overlayscrollbars" has visible scroll.

--- src/client/shared/ui/sidebar/sidebar-body/SidebarBody.tsx
+++ src/client/shared/ui/sidebar/sidebar-body/SidebarBody.tsx
@@ -1,15 +1,15 @@
+import styles from './SidebarBody.module.scss';
+
 import React from 'react';
 
 type Props = {
   children: React.ReactNode
 };
 
-const SidebarBody = ({ children }: Props) => {
-  return (
-    <div className="flex-grow-1">
-      {children}
-    </div>
-  );
-}
+const SidebarBody = ({ children }: Props) => (
+  <div className={styles.element}>
+    {children}
+  </div>
+);
 
 export default SidebarBody;
